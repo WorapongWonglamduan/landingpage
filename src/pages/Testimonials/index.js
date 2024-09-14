e770c3bd1bf20f7083cffc680754a788332fe425
WorapongWonglamduan <the_oo_ooo@hotmail.com>
2024-09-14 15:41:32 +0700

    update

--- src/pages/Testimonials/index.js
+++ src/pages/Testimonials/index.js
@@ -3,12 +3,32 @@ import "./index.css";
 const index = () => {
   return (
     <section className="testimonials">
-      <h2>What Our Customers Say</h2>
-      <div className="testimonial">
-        <p>"Best food delivery app I've ever used!" - Jane Doe</p>
-      </div>
-      <div className="testimonial">
-        <p>"Quick and reliable service, highly recommend." - John Smith</p>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src={require("../../assets/image/food-9.png")}
+            alt="Food"
+            className="img"
+          />
+        </div>
+        <div className="col-6">
+          <h4>Lorem ipsum</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Accusantium, architecto consectetur consequatur debitis deserunt
+            laudantium minus perferendis possimus, quo recusandae rem, vel
+            veritatis! Iste, labore, laboriosam? Consectetur debitis illo, in
+            necessitatibus neque nobis officia sequi.
+          </p>
+          <div className="wrap-profile">
+            <img
+              src={require("../../assets/image/food-9.png")}
+              alt="Food"
+              className="profile-img"
+            />
+            <h4>worapong</h4>
+          </div>
+        </div>
       </div>
     </section>
   );
